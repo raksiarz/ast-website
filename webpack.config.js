27b885285952ab9c1b187a50943e5b0ea8d39a0a
raksiarz <raksiarz@gmail.com>
2024-11-08 18:01:54 +0100

    adding style-loader

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
             },
             {
                 test: /\.css$/i,
-                use: 'css-loader',
+                use: ['style-loader', 'css-loader'],
             }
         ]
     },

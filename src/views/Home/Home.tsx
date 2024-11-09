21c659640e2b2c6ab20e44dedf45c81161bc7ef6
raksiarz <raksiarz@gmail.com>
2024-11-09 14:58:23 +0100

    routing fix

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -7,8 +7,8 @@ const Home = () => {
             <h1 className='title'>Monkeylang</h1>
             <p className='about'>This is unofficial documentation of Monkeylang.</p>
             <div className='navigation-container'>
-                <Link className='navigation-button' to={'documentation'}>Docs</Link>
-                <Link className='navigation-button' to={'editor'}>Playground</Link>
+                <Link className='navigation-button' to={'/documentation'}>Docs</Link>
+                <Link className='navigation-button' to={'/editor'}>Playground</Link>
             </div>
         </div>
     )
